949ce5a98271f8253067657cff7ea84cf97dbb3f
juliana.santos <juliana.santos@db1.com.br>
2025-06-13 15:16:10 -0300

    init commit

---
+++ src/main/java/com/code/craft/mentor/dev/CodeCraftMentorDevApplication.java
@@ -0,0 +1,13 @@
+package com.code.craft.mentor.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeCraftMentorDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeCraftMentorDevApplication.class, args);
+    }
+
+}
